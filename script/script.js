aa71fad8b7ad7b30e54ba8fb5f03ce5060b446bb
Danil Maiorov <damayorov93@gmail.com>
2022-08-16 06:11:49 +0300

    changes

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-const langPage = document.querySelectorAll('.lang-page')
-const swithcer = document.querySelectorAll('.input-checkbox')
+const langPage = document.querySelectorAll('.lang-page');
+const swithcer = document.querySelectorAll('.input-checkbox');
 
 swithcer.forEach(item => {
     item.addEventListener('click', () => {
         if(!item.checked) {
-            item.removeAttribute('checked')
+            item.removeAttribute('checked');
             langPage.forEach(page => {
                 if(!item.checked) {
-                    page.classList.toggle('hide')
-                    page.classList.toggle('visible')
+                    page.classList.toggle('hide');
+                    page.classList.toggle('visible');
                 }
             })
         } else {
-            item.setAttribute('checked', '')
+            item.setAttribute('checked', '');
             if(item.checked) {
                 langPage.forEach(page => {
-                    page.classList.toggle('hide')
-                    page.classList.toggle('visible')
-                })
+                    page.classList.toggle('hide');
+                    page.classList.toggle('visible');
+                });
             }
         }
-    })
-})
+    });
+});
